3ccd6e788df559fb6e78ae20cc0b3c34c94ee680
lukec <lukec@4ee4aff5-2028-0410-b109-97ae9d850f25>
2007-04-04 23:16:57 +0000

    Add devblogs code
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext-clients/trunk@126 4ee4aff5-2028-0410-b109-97ae9d850f25

---
+++ rest/perl/Socialtext-Blikistan-DevBlogs/lib/Socialtext/Blikistan/DevBlogs.pm
@@ -0,0 +1,37 @@
+package Socialtext::Blikistan::DevBlogs;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+use Apache2::RequestRec ();
+use Apache2::RequestIO ();
+
+use Apache2::Const -compile => qw(OK);
+use Socialtext::Resting::Getopt qw/get_rester/;
+use Blikistan;
+
+sub handler {
+    my $r = shift;
+
+    my $rester = get_rester(
+        'rester-config' => '/home/lukec/devblogs/stoss-rester.conf',
+    );
+    die "no server!" unless $rester->server; 
+
+    my %magic_opts;
+    if ($r->uri =~ m#^/(\w+)#) {
+        $magic_opts{subblog} = $1;
+    }
+    my $b = Blikistan->new(
+        rester => $rester,
+        magic_opts => \%magic_opts,
+    );
+
+    $r->content_type('text/html');
+    print $b->print_blog;
+
+    return Apache2::Const::OK;
+}
+
+1;
