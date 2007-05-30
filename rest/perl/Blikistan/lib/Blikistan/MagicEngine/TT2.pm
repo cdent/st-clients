5f106e9c78c3006bae521c1ceee0d015f8498d7b
lukec <lukec@4ee4aff5-2028-0410-b109-97ae9d850f25>
2007-05-30 08:19:21 +0000

    add a Simple magic engine, and move common functions into TT2
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext-clients/trunk@185 4ee4aff5-2028-0410-b109-97ae9d850f25

--- rest/perl/Blikistan/lib/Blikistan/MagicEngine/TT2.pm
+++ rest/perl/Blikistan/lib/Blikistan/MagicEngine/TT2.pm
@@ -4,11 +4,15 @@ use warnings;
 use base 'Blikistan::MagicEngine';
 use Template;
 use FindBin;
+use URI::Escape;
+use JSON;
 
 sub render_template {
     my $self = shift;
-    my $params = shift;
+    my $params = $self->{params} = shift || {};
     my $r = $self->{rester};
+
+    $self->load_rester_utils;
 
     $params->{magic_engine} = ref($self);
     my $output = '';
@@ -49,6 +53,70 @@ Could not find template at $self->{template_page}
 </body>
 </html>
 EOT
+}
+
+sub load_rester_utils {
+    my $self = shift;
+    my $r = $self->{rester};
+
+    my $show_latest = delete $self->{params}{show_latest_posts}
+        || $self->{show_latest_posts};
+
+    $self->{params}{tagged_pages} ||= sub {
+        my $tag = shift;
+        $r->accept('text/plain');
+        my @posts = $r->get_taggedpages($tag);
+        @posts = splice @posts, 0, $show_latest;
+
+        $r->accept('text/html');
+        return [ map { $self->_load_page($_) } @posts ];
+    };
+
+    $self->{params}{show_page} ||= sub {
+        my $p = $self->_load_page(shift);
+        return $p->{html};
+    };
+
+    $self->{params}{workspace_tags} ||= sub {
+        my $skip = shift || '';
+        $r->accept('application/json');
+        my $tags = jsonToObj($r->get_workspace_tags);
+        $tags = [ 
+            sort { $b->{page_count} <=> $a->{page_count} } 
+                grep { $_->{name} ne $skip }
+                    @$tags 
+        ];
+        return $tags;
+    };
+
+    $self->{params}{abbrev_page} ||= sub {
+        my $page   = shift;
+        my $length = shift || 30;
+        my $p = $self->_load_page($page);
+        return substr($p->{wikitext}, 0, $length);
+    }
+}
+
+sub linkify {
+    my $self = shift;
+
+    my $p = $self->_load_page(shift);
+    return $p->{page_uri};
+}
+
+sub _load_page {
+    my $self = shift;
+    my $page = uri_escape(shift);
+    return $self->{_page}{$page} if $self->{_page}{$page};
+
+    my $r    = $self->{rester};
+    $r->accept('application/json');
+    $r->json_verbose(1);
+    my $p = $self->{_page}{$page} = jsonToObj( $r->get_page($page) );
+    while ($p->{html} =~ s/<a href="([\w_]+)"\s*/'<a href="' . $self->linkify($1) . '"'/eg) {}
+
+    $p->{html} =~ s#^<div class="wiki">(.+)</div>\s*$#$1#s;
+    return $p;
 }
 
 1;
