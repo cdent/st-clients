c7af5126d44c51b848f4df66d89d27d910d76efc
kirsten <kirsten@4ee4aff5-2028-0410-b109-97ae9d850f25>
2007-08-01 17:11:02 +0000

    Updating Hydra and adding the NewsBlog handler.  Not finished but touchstone.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext-clients/trunk@224 4ee4aff5-2028-0410-b109-97ae9d850f25

--- rest/perl/Socialtext-Blikistan-Hydra/lib/Socialtext/Blikistan/Hydra.pm
+++ rest/perl/Socialtext-Blikistan-Hydra/lib/Socialtext/Blikistan/Hydra.pm
@@ -20,26 +20,26 @@ sub handler {
     my %magic_opts;
 
     if ($r->uri =~ m#^/hydra/search/pages#) {
-	my %args = $r->args;
-	my $search = $args{'q'};
-	$magic_opts{search} = $search;
+        my %args = $r->args;
+        my $search = $args{'q'};
+        $magic_opts{search} = $search;
     } elsif ($r->uri =~ m#^/hydra/search/([^/]+)$#) {
-	$magic_opts{search} = $1;
-    } elsif ($r->uri =~ m#^/hydra/(\w+)#) {
+        $magic_opts{search} = $1;
+    } elsif ($r->uri =~ m#^/hydra/(.*)$#) {
         $magic_opts{subpage} = $1;
     }
 
     my $b = Blikistan->new(
-	magic_engine => 'perlSite',
+        magic_engine => 'perlSite',
         rester => $rester,
         magic_opts => \%magic_opts,
     );
-
-    $r->content_type('text/html');
-    $r->status_line("201 Ok");
-    $r->send_http_header();
-    $r->print ($b->print_blog);
-
+    if ( my $content = $b->print_blog ) {
+    	$r->content_type('text/html');
+    	$r->status_line("201 Ok");
+    	$r->send_http_header();
+    	$r->print ($content);
+    }
     return OK;
 }
 
